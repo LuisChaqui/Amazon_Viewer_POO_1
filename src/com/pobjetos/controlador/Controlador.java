959ec6024c2b6be18a7740ac069dcd6cb68ffe1c
LuisChaqui <luigi92pm@gmail.com>
2018-10-30 18:23:31 -0500

    primera rama

--- src/com/pobjetos/controlador/Controlador.java
+++ src/com/pobjetos/controlador/Controlador.java
@@ -5,5 +5,7 @@ package com.pobjetos.controlador;
  * @author SSolis
  */
 public class Controlador {
-
+    public static void main(String[] args) {
+        System.out.println("Ramificacion prueba 1");
+    }
 }
